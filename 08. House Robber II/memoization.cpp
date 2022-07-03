0e6ad71f4d59ca3d006bba152ccde822d53fdf32
Loganathan <loganvk18@gmail.com>
2022-07-03 23:39:22 +0530

    tabulation not done

---
+++ 08. House Robber II/memoization.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int houseRob(int i,vector<int> &nums,vector<int> &dp){
+    if(i<0)return 0;
+    if(dp[i]!=-1)return dp[i];
+    if(i==0)return nums[0];
+    return dp[i]=max(nums[i]+houseRob(i-2,nums,dp),houseRob(i-1,nums,dp));
+}
+int rob(vector<int>& nums) {
+    if(nums.size()==1)return nums[0];
+    vector<int> dp1(nums.size(),-1),dp2(nums.size(),-1);
+    vector<int> v1(nums.begin(),nums.end()-1);
+    vector<int> v2(nums.begin()+1,nums.end());
+    return max(houseRob(v1.size(),v1,dp1),houseRob(v2.size(),v2,dp2));
+}
+int main(){
+    return 0;
+}
---
+++ 08. House Robber II/tabulation.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rob(vector<int>& nums) {
+
+}
+int main(){
+
+    return 0;
+}
