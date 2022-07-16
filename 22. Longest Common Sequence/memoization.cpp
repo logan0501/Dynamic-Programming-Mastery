ba0b0aaa46a5bee0fa2cd218d1e25a5c78c32810
Loganathan <loganvk18@gmail.com>
2022-07-16 22:41:37 +0530

    lcs tabulaition completed

--- 22. Longest Common Sequence/memoization.cpp
+++ 22. Longest Common Sequence/memoization.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+//shifting by 1 
 int f(int i,int j,string s,string t,  vector<vector<int>> &dp){
-    if(i==-1 || j==-1)return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
+    if(i==0 || j==0)return 0;
+    if(dp[i-1][j-1]!=-1)return dp[i-1][j-1];
     int val=0;
-    if(s[i]==t[j])val = 1 + f(i-1,j-1,s,t,dp);
+    if(s[i-1]==t[j-1])val = 1 + f(i-1,j-1,s,t,dp);
     else val = max(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp));
-    return dp[i][j]=val;
+    return dp[i-1][j-1]=val;
 }
 int lcs(string s, string t)
 {
     vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
-	return f(s.size()-1,t.size()-1,s,t,dp);
+	return f(s.size(),t.size(),s,t,dp);
 }
 int main(){
 
--- 22. Longest Common Sequence/tabulation.cpp
+++ 22. Longest Common Sequence/tabulation.cpp
@@ -1,7 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int lcs(string s, string t)
+{
+    vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
 
-int main(){
+    for (int i = 1; i <= s.size(); i++)
+    {
+        for (int j = 1; j <= t.size(); j++)
+        {
+            int val = 0;
+            if (s[i - 1] == t[j - 1])
+                val = 1 + dp[i - 1][j - 1];
+            else
+                val = max(dp[i - 1][j], dp[i][j - 1]);
+            dp[i][j] = val;
+        }
+    }
+    return dp[s.size()][t.size()];
+}
+int main()
+{
 
     return 0;
 }
